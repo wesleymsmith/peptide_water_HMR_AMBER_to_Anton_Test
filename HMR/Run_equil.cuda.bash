8c2bd42ede77a718c3bde22b75f54f38b4596a33
Wesley Botello-Smith <wmsmith@uci.edu>
2018-04-18 15:15:38 -0700

    Branched HMR and ran equil. Generated ascii restart files

---
+++ HMR/Run_equil.cuda.bash
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH -J PzYdProd
+#SBATCH --get-user-env
+#SBATCH --partition=gpus
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=300:00:00
+#SBATCH --mail-user=wesley
+
+#set -e 
+
+source /cm/shared/apps/amber16_cuda/amber.sh
+
+set -e
+toppath="step2_charmm2amber.parm7"
+
+#echo "running production"
+#step="step2_charmm2amber"
+#for ii in `seq -f "%03g" 0 1 0`
+#do
+#	prev_step=$step
+#	step="step3_minimization"
+#	echo "running $step"
+#	$AMBERHOME/bin/pmemd.cuda  -O -i $step.mdin -c $prev_step.rst7 \
+#		-p $toppath -r $step.rst7 -ref $prev_step.rst7 \
+#		-o $step.out -x $step.nc
+#done
+
+step=step4.4_equilibration
+for ii in `seq 5 1 5`
+do
+	prev_step=$step
+	step="step4.${ii}_equilibration"
+	echo "running $step"
+	$AMBERHOME/bin/pmemd.cuda  -O -i $step.mdin -c $prev_step.rst7 \
+		-p $toppath -r $step.rst7 -ref $prev_step.rst7 \
+		-o $step.out -x $step.nc
+done
+
+echo "done"
+
+
